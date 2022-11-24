d68723d8d6165b2ccefc34ab2a978f1f4177c306
paolotti00 <paolo.tripodi@aubay.it>
2022-11-24 01:47:58 +0100

    retrieveDeviceById impl service , done

--- src/main/java/com/paolotti/my/smart/home/service/IDeviceService.java
+++ src/main/java/com/paolotti/my/smart/home/service/IDeviceService.java
@@ -8,9 +8,9 @@ import com.paolotti.my.smart.home.model.DeviceActionsSchema;
 import java.util.ArrayList;
 
 public interface IDeviceService {
-    public void doFwActionsSchema(Device device, DeviceActionsSchema deviceActionsSchema);
-    public ArrayList<DeviceActionsSchema> getSupportedActionsSchemas(Device device);
-    public void doCustomActionSchema(Device device, DeviceActionsSchema deviceActionsSchema);
+    public void doFwActionsSchema(String deviceId, DeviceActionsSchema deviceActionsSchema);
+    public ArrayList<DeviceActionsSchema> getSupportedActionsSchemas(String deviceId);
+    public void doCustomActionSchema(String deviceId, DeviceActionsSchema deviceActionsSchema);
     public Device retrieveDeviceById(String deviceId) throws DeviceNotExistsException;
     public ArrayList<Device> retrieveDevicesByGroupId(String groupId) throws GroupNotExistsException;
 }
---
+++ src/main/java/com/paolotti/my/smart/home/service/impl/DeviceServiceImpl.java
@@ -0,0 +1,58 @@
+package com.paolotti.my.smart.home.service.impl;
+
+import com.paolotti.my.smart.home.exception.DeviceNotExistsException;
+import com.paolotti.my.smart.home.exception.GroupNotExistsException;
+import com.paolotti.my.smart.home.mapper.IDeviceMapper;
+import com.paolotti.my.smart.home.model.Device;
+import com.paolotti.my.smart.home.model.DeviceActionsSchema;
+import com.paolotti.my.smart.home.repository.IDeviceCustomRepository;
+import com.paolotti.my.smart.home.repository.entity.DeviceEntity;
+import com.paolotti.my.smart.home.service.IDeviceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+public class DeviceServiceImpl implements IDeviceService {
+    @Autowired
+    IDeviceCustomRepository deviceCustomRepository;
+    @Autowired
+    IDeviceMapper deviceMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(DeviceServiceImpl.class);
+
+    @Override
+    public void doFwActionsSchema(String deviceId, DeviceActionsSchema deviceActionsSchema) {
+
+    }
+
+    @Override
+    public ArrayList<DeviceActionsSchema> getSupportedActionsSchemas(String deviceId) {
+        return null;
+    }
+
+    @Override
+    public void doCustomActionSchema(String deviceId, DeviceActionsSchema deviceActionsSchema) {
+
+    }
+
+    @Override
+    public Device retrieveDeviceById(String deviceId) throws DeviceNotExistsException {
+        logger.info("retrieving device with id {}", deviceId);
+        DeviceEntity deviceEntity = deviceCustomRepository.findById(deviceId);
+        if (deviceEntity == null) {
+            logger.warn("no device found with {} id", deviceId);
+            throw new DeviceNotExistsException(deviceId);
+        }
+        logger.info("device {} retrieved, converting to model", deviceId);
+        Device device = deviceMapper.toModel(deviceEntity);
+        logger.info("device converted to {}", device);
+        return device;
+    }
+
+    @Override
+    public ArrayList<Device> retrieveDevicesByGroupId(String groupId) throws GroupNotExistsException {
+        return null;
+    }
+}
